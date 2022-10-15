79d3430f2ebd5706cfe8e707d2df44aa30770e5e
Yusuf Çavdar <89012489+ycyusufcavdar@users.noreply.github.com>
2022-10-15 16:49:01 +0300

    Add files via upload

---
+++ net-core/try-catch/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Bu ders dosyası net core 6.0 ile oluşturulmuştur.
+
+// try
+// {
+//     // Hata ihtimalı olan kod bloğu.
+//     Console.WriteLine("Bir sayı giriniz");
+//     int sayi = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Girmiş olduğunuz sayı:" + sayi);
+// }
+// catch(System.Exception ex)
+// {
+//     // Hata olursa yapılacak işlem
+//     Console.WriteLine("Hata: " + ex.Message.ToString());
+// }
+// finally
+// {
+//     // Hata olsun ya da olmasın, işlemin son aşaması. Ancak burası opsiyonel.
+//     Console.WriteLine("İşlem tamamlandı");
+// }
+
+
+try
+{
+    //int a = int.Parse(null);
+    //int a = int.Parse("test");
+    int a = int.Parse("-2000000000000000");
+}
+catch (System.ArgumentNullException ex)
+{
+    Console.WriteLine("Boş değer girdiniz.");
+    Console.WriteLine(ex);
+}
+catch (System.FormatException ex)
+{
+    Console.WriteLine("Veri tipi uygun değil.");
+    Console.WriteLine(ex);
+}
+catch (System.OverflowException ex)
+{
+    Console.WriteLine("Çok küçük ya da çok büyük bir değer girdiniz.");
+    Console.WriteLine(ex);
+}
+finally
+{
+    Console.WriteLine("İşlem başarıyla tamamlanmıştır.");
+}
---
+++ net-core/while-foreach/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+// NET CORE 6.0
+
+
+//while
+// 1'den başlayarak console dan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console a yazma programı. 
+// Console.WriteLine("Lütfen bir sayı giriniz: ");
+// int sayi = int.Parse(Console.ReadLine());
+// int sayac = 1;
+// int toplam = 0;
+// while (sayac <= sayi)
+// {
+//     toplam += sayac;
+//     sayac++;
+// }
+// Console.WriteLine(toplam/sayi);
+
+// // a'dan z'ye kadar tüm harfleri console a yazdır.
+
+// char character = 'a';
+// while (character <= 'z')
+// {
+//     Console.Write(character);
+//     character ++;
+// }
+
+Console.WriteLine("*****FOREACH*****");
+
+string[] arabalar = {"bmw", "ford","toyota","nissan"};
+
+foreach (var araba in arabalar)
+{
+    Console.WriteLine(araba);
+}
