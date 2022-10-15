77663dd234a4280057f1e58034c0d6c06d3fd303
Yusuf Çavdar <89012489+ycyusufcavdar@users.noreply.github.com>
2022-10-15 16:45:14 +0300

    Add files via upload

---
+++ net-core/metotlar-overloading/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+// NET CORE 6.0
+
+string sayi = "999";
+
+bool sonuc = int.TryParse(sayi, out  int outSayi);
+
+if(sonuc)
+{
+    Console.WriteLine("Başarılı");
+    Console.WriteLine(outSayi);
+}
+else
+{
+    Console.WriteLine("Başarısız");
+}
+
+Metotlar instance = new Metotlar();
+instance.Topla(4,5, out int toplamSonucu);
+Console.WriteLine(toplamSonucu);
+
+// METOT AŞIRI YÜKLEME - OVERLOADİNG
+int ifade = 999;
+instance.ekranaYazdir(Convert.ToString(ifade));
+instance.ekranaYazdir(ifade);
+
+instance.ekranaYazdir("Yusuf ", "Çavdar");
+
+// Metot imzası 
+// Metotadı + parametre sayısı + parametre tipi
+
+
+class Metotlar
+{
+    public void Topla(int a , int b, out int toplam)
+    {
+        toplam = a+b;
+    }
+
+    public void ekranaYazdir(string veri )
+    {
+        Console.WriteLine(veri);
+    }
+
+    public void ekranaYazdir(int veri )
+    {
+        Console.WriteLine(veri);
+    }
+
+    public void ekranaYazdir(string veri1 , string veri2 )
+    {
+        Console.WriteLine(veri1 + veri2);
+    }
+
+}
---
+++ net-core/operatorler/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Bu proje dökümanı net 6.0 ile oluşturulmuştur.
+
+// Atama ve işlemli Atama
+Console.WriteLine("*****Atama ve İşlemli Atama*****");
+int x = 3;
+int y = 3;
+
+y = y+2;
+Console.WriteLine(y);
+
+y+=2;
+Console.WriteLine(y);
+y/=1;
+Console.WriteLine(y);
+x*=2;
+Console.WriteLine(x);
+
+
+// Mantıksal Operatörler 
+// && ve, || veya, ! değil
+Console.WriteLine("*****Mantıksal Operatörler*****");
+
+bool isSuccess = true;
+bool isCompleted = false;
+
+if(isSuccess && isCompleted){
+    Console.WriteLine("Perfect");
+}
+if(isSuccess || isCompleted){
+    Console.WriteLine("Great");
+}
+if(isSuccess && !isCompleted){
+    Console.WriteLine("Fine");
+}
+
+// İlişkisel Operatörler
+// <, >, <=, >=, ==, !=
+Console.WriteLine("*****İlişkisel Operatörler*****");
+
+int a =3;
+int b =4;
+bool sonuc = a<b;
+Console.WriteLine(sonuc);
+sonuc = a>b;
+Console.WriteLine(sonuc);
+sonuc = a>=b;
+Console.WriteLine(sonuc);
+sonuc = a<=b;
+Console.WriteLine(sonuc);
+sonuc = a==b;
+Console.WriteLine(sonuc);
+sonuc = a!=b;
+Console.WriteLine(sonuc);
+
+// Aritmetik Operatörler
+// /, *, -, +, %
+
+Console.WriteLine("*****Aritmetik Operatörler*****");
+
+int sayi1= 10;
+int sayi2= 5;
+int sonuc1 = sayi1/sayi2;
+Console.WriteLine("Bölme işlemi sonucu: " +sonuc1);
+sonuc1= sayi1*sayi2;
+Console.WriteLine("Çarpma işlemi sonucu: " +sonuc1);
+sonuc1 = sayi1-sayi2;
+Console.WriteLine("Çıkarma işlemi sonucu:  "+sonuc1);
+sonuc1 = sayi1+sayi2;
+Console.WriteLine("Toplama işlemi sonucu: " +sonuc1);
+sonuc1 = ++sayi1;
+Console.WriteLine("Sayıyı 1 ile toplama işlemi:" +sonuc1);
+
+// Mod alma işlemi
+int sonuc2 = 24%5;
+Console.WriteLine("Mod alma sonucu:  " +sonuc2);
