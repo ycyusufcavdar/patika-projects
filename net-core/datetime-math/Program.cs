b7263bbed6fb2074489c8f89a191025f977b5cca
Yusuf Çavdar <89012489+ycyusufcavdar@users.noreply.github.com>
2022-10-15 16:41:57 +0300

    Add files via upload

---
+++ net-core/datetime-math/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+// NET CORE 6.0
+
+Console.WriteLine(DateTime.Now); // günün tarihini getirir.
+Console.WriteLine(DateTime.Now.Date);
+Console.WriteLine(DateTime.Now.Day);
+Console.WriteLine(DateTime.Now.Month);
+Console.WriteLine(DateTime.Now.Year);
+Console.WriteLine(DateTime.Now.Hour);
+Console.WriteLine(DateTime.Now.Minute);
+Console.WriteLine(DateTime.Now.Second);
+
+
+Console.WriteLine(DateTime.Now.DayOfWeek); // haftanın gününü string getirir
+Console.WriteLine(DateTime.Now.DayOfYear); // İçinde bulunulan yılın kaçıncı gününde olduğunu getirir
+
+
+Console.WriteLine(DateTime.Now.ToLongDateString());  // gün ay yıl ama yazıyla
+Console.WriteLine(DateTime.Now.ToShortDateString()); // gün ay yıl ama sayıyla
+
+
+Console.WriteLine(DateTime.Now.ToLongTimeString()); // saat dakika saniye
+Console.WriteLine(DateTime.Now.ToShortTimeString()); // saat dakika
+
+
+Console.WriteLine(DateTime.Now.AddDays(2)); // tarihe 2 gün ekledi
+Console.WriteLine(DateTime.Now.AddHours(3));
+Console.WriteLine(DateTime.Now.AddSeconds(30));
+Console.WriteLine(DateTime.Now.AddMonths(5));
+Console.WriteLine(DateTime.Now.AddYears(10));
+Console.WriteLine(DateTime.Now.AddMilliseconds(50));
+
+
+Console.WriteLine("***** Datetime Format ******");
+Console.WriteLine(DateTime.Now.ToString("dd")); // 24
+Console.WriteLine(DateTime.Now.ToString("ddd")); // sat
+Console.WriteLine(DateTime.Now.ToString("dddd")); // saturday
+
+Console.WriteLine(DateTime.Now.ToString("MM")); //04
+Console.WriteLine(DateTime.Now.ToString("MMM")); // apr
+Console.WriteLine(DateTime.Now.ToString("MMMM")); // april
+
+
+Console.WriteLine(DateTime.Now.ToString("yy")); //22
+Console.WriteLine(DateTime.Now.ToString("yyyy")); // 2022
+
+
+Console.WriteLine("**** Math Kütüphanesi ****");
+Console.WriteLine(Math.Abs(-25)); // 25
+Console.WriteLine(Math.Sin(10)); // sinus hesabı
+Console.WriteLine(Math.Cos(10)); // cosinus
+Console.WriteLine(Math.Tan(10)); // tanjant
+
+
+Console.WriteLine(Math.Ceiling(22.3)); // 22.3'den büyük en küçük tam sayıyı getirir
+Console.WriteLine(Math.Round(22.3)); // hangi tarafa yakınsa onu getirir
+Console.WriteLine(Math.Round(22.7)); 
+Console.WriteLine(Math.Floor(22.7)); // 22.7'den küçük en büyük tam sayıyı getirir
+
+
+Console.WriteLine(Math.Max(2,6)); // 6 getirir
+Console.WriteLine(Math.Min(2,6)); // 2 getirir 
+Console.WriteLine(Math.Pow(3,4)); // 3 üzeri 4'ü verir
+Console.WriteLine(Math.Sqrt(9)); // karekök alır
+Console.WriteLine(Math.Log(9)); // 9 un e tabanındaki karşılığı
+Console.WriteLine(Math.Exp(3)); // e üzeri 3ü getirir
+Console.WriteLine(Math.Log10(10)); // 10 sayısının log10 tabanındaki karşılığı
---
+++ net-core/degiskenler/Program.cs
@@ -0,0 +1,92 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Bu çalışmada dotnet version 6 kullanılmıştır. 
+using System;
+
+namespace degisken
+{
+
+    class Program{
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            byte b = 3 ;     // 1 byte
+            sbyte c = 3;    // 1 byte
+            
+            short s = 3;    // 2 byte
+            ushort us = 3;  // 2 byte
+
+            Int16 i16 = 2;  // 2 byte
+            int i = 2;      // 4 byte
+            Int32 i32 = 2;  // 4 byte
+            Int64 i64 = 2;  // 8 byte
+
+            uint ui = 2 ;   // 4 byte
+            long l = 4 ;    // 8 byte
+            ulong ul = 4;   // 8 byte
+
+            float f = 3;    // 4 byte
+            double d = 3 ;  // 8 byte
+            decimal de = 3; // 16 byte
+            
+            char ch = '2';  // 2 byte 
+            string str = "Yusuf Çavdar";     // sınırsız
+
+            bool b1 = true;
+            bool b2 = false ;
+
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(dt);
+
+            object o1 = "x";
+            object o2 = 'y';
+            object o3 = 4 ;
+            object o4 = 4.3;
+
+            // string ifadeler
+
+            string str1 = string.Empty;
+            str1 = "Yusuf Çavdar";
+            string ad = "Yusuf";
+            string soyad = "Çavdar";
+            string tamIsim = ad + " " + soyad ;
+
+            // integer tanımlama şekilleri
+
+            int integer1 = 5;
+            int integer2 = 3;
+            int integer3 = integer1 * integer2;
+
+            // boolean
+
+            bool bool1 = 10>2;
+
+            // değişken dönüşümleri
+
+            string str20 = "20";
+            int int20 = 20;
+
+            string yeniDeger = str20 + int20.ToString();
+            Console.WriteLine(yeniDeger); // çıktısı 2020
+
+            int int21 = int20 + Convert.ToInt32(str20);
+            Console.WriteLine(int21); // çıktısı 40
+
+            int int22 = int20 + int.Parse(str20); /// çıktısı 40
+
+            // dateTime
+
+            string datetime = DateTime.Now.ToString("dd.MM.yyyy");
+            Console.WriteLine(datetime);
+
+            string datetime2 = DateTime.Now.ToString("dd/MM/yyyy");
+            Console.WriteLine(datetime2);
+
+            string hour = DateTime.Now.ToString("HH:mm:ss");
+            Console.WriteLine(hour);
+
+        }
+    }
+
+}
