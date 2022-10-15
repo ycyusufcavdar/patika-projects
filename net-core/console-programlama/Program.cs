d82aa20a43f2e2576892fe92aa8c731a56e24017
Yusuf Cavdar <yc.yusufcavdar@gmail.com>
2022-10-15 16:37:02 +0300

    klasor yapisi duzenleniyor

--- net-core/console-programlama/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("İsminizi Girin");
-string name = Console.ReadLine();
-Console.WriteLine("Soyisminizi Girin");
-string surname = Console.ReadLine();
-Console.WriteLine("Merhaba" + name + " " + surname);
